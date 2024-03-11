a5ead554e896425dab005b33035c9daf7cd67ef5
Md Nazmul Hassan <162824391+nazmul-475271@users.noreply.github.com>
2024-03-11 08:57:00 +0000

    03 days

--- 01_basics/03.js
+++ 01_basics/03.js
@@ -2,10 +2,10 @@
 let score = "33ads"
 //finding the type of value
 console.log(typeof score)
-console.log(typeof(score));
+console.log(typeof (score));
 
 //convert string to number .
-let ValuInNumber=Number(score)
+let ValuInNumber = Number(score)
 console.log(typeof ValuInNumber);
 // NaN= Not a Number(because of "33adc" is not a pure number. but converting 
 // the string into a number it's convert the string to 
