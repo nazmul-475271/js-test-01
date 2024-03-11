9956c56b6452d5744645586736e52cfd8e69d15d
Md Nazmul Hassan <162824391+nazmul-475271@users.noreply.github.com>
2024-03-11 08:51:23 +0000

    day3practice

---
+++ 01_basics/03.js
@@ -0,0 +1,13 @@
+// node 01_basics/03.js
+let score = "33ads"
+//finding the type of value
+console.log(typeof score)
+console.log(typeof(score));
+
+//convert string to number .
+let ValuInNumber=Number(score)
+console.log(typeof ValuInNumber);
+// NaN= Not a Number(because of "33adc" is not a pure number. but converting 
+// the string into a number it's convert the string to 
+// a number but cant compleatly change it it's an inbuilt error)
+console.log(ValuInNumber)
