1c5c2bca790d990d21eeeddb921473f6e08d45a4
Md Nazmul Hassan <162824391+nazmul-475271@users.noreply.github.com>
2024-03-12 09:02:44 +0000

    conversation

---
+++ 01_basics/04.js
@@ -0,0 +1,11 @@
+//node 01_basics/04.js
+console.log(null==0)
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+// -===
+console.log("2"===2)
