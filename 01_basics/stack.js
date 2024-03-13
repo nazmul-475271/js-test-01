9bc308b04429d42c3c9f1de63f1fc09beff736e3
Md Nazmul Hassan <162824391+nazmul-475271@users.noreply.github.com>
2024-03-13 11:12:04 +0000

    stack&heap

---
+++ 01_basics/stack.js
@@ -0,0 +1,11 @@
+//stack and heap
+//stack(Primitive) & Heap(non-Primitive)
+//Heap= (Array, Function, Object)
+// node 01_basics/stack.js
+let MyValue = "Amar code"
+let AnotherValue = MyValue
+console.log(AnotherValue);
+
+AnotherValue="ami nazmul"
+console.log(AnotherValue);
+console.log(MyValue);
--- 06sh.js
+++
@@ -1,7 +0,0 @@
-//stack and heap
-//stack (Primitive) and heap(non-Primitive)
-//heap=(array,function, object)
-// node 01_basics/06sh.js
-let Valueis =  "nazmul"
-let NewValue = Valueis
-console.log(NewValue);
